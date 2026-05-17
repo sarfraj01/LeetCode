fd672469b1d5278195db35d817c9b0ca47cbdf12
sarfraj01 <135052506+sarfraj01@users.noreply.github.com>
2026-05-17 17:08:44 +0530

    Time: 36 ms (86.79%) | Memory: 234 MB (66.88%) - LeetSync

--- 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
+++ 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
@@ -47,11 +47,47 @@ public:
             return dp[currlane][currpos];
         }
     }
+
+    //Tabulation
+    int solveTab(vector<int> &obstacles){
+        int n = obstacles.size()-1;
+        vector<vector<int> > dp(4 , vector<int>(obstacles.size() , 1e9));
+
+        dp[0][n] = 0;
+        dp[1][n] = 0;
+        dp[2][n] = 0;
+        dp[3][n] = 0;
+
+        for(int currpos = n-1; currpos >= 0; currpos--){
+            for(int currlane = 1; currlane <= 3; currlane++){
+
+        if(obstacles[currpos + 1] != currlane){
+            dp[currlane][currpos] = dp[currlane][currpos + 1];
+        }
+
+        else{
+
+            //sideway jumps
+            int ans = 1e9;
+            for(int i = 1; i <= 3; i++){
+            if(currlane != i && obstacles[currpos] != i)
+                ans = min(ans , 1 + dp[i][currpos+1]);
+            }
+            dp[currlane][currpos] = ans;
+            }
+
+        }
+    }
+    return min(dp[2][0] , min(1+dp[1][0] , 1 + dp[3][0]));
+
+}
     int minSideJumps(vector<int>& obstacles) {
         //return solve(obstacles , 2 , 0);
 
         //Memorization
-        vector<vector<int> > dp(4 , vector<int> (obstacles.size() , -1));
-        return solveMem(obstacles , 2 , 0 , dp);
+        // vector<vector<int> > dp(4 , vector<int> (obstacles.size() , -1));
+        // return solveMem(obstacles , 2 , 0 , dp);
+
+        return solveTab(obstacles);
     }
 };
