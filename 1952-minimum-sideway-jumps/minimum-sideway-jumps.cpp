0bace3c4e45c3e819760362d9e691d43dcae0dbe
sarfraj01 <135052506+sarfraj01@users.noreply.github.com>
2026-05-18 11:29:48 +0530

    Time: 67 ms (55.92%) | Memory: 191.9 MB (82.66%) - LeetSync

--- 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
+++ 1952-minimum-sideway-jumps/minimum-sideway-jumps.cpp
@@ -81,6 +81,43 @@ public:
     return min(dp[2][0] , min(1+dp[1][0] , 1 + dp[3][0]));
 
 }
+
+//Space optimization
+    int solveSpace(vector<int> &obstacles){
+        int n = obstacles.size()-1;
+
+        vector<int> curr(4,INT_MAX);
+        vector<int> next(4,INT_MAX);
+
+        next[0] = 0;
+        next[1] = 0;
+        next[2] = 0;
+        next[3] = 0;
+
+        for(int currpos = n-1; currpos >= 0; currpos--){
+            for(int currlane = 1; currlane <= 3; currlane++){
+
+        if(obstacles[currpos + 1] != currlane){
+            curr[currlane] = next[currlane];
+        }
+
+        else{
+
+            //sideway jumps
+            int ans = 1e9;
+            for(int i = 1; i <= 3; i++){
+            if(currlane != i && obstacles[currpos] != i)
+                ans = min(ans , 1 + next[i]);
+            }
+            curr[currlane] = ans;
+            }
+
+        }
+        next = curr;
+    }
+    return min(next[2] , min(1+next[1] , 1 + next[3]));
+
+}
     int minSideJumps(vector<int>& obstacles) {
         //return solve(obstacles , 2 , 0);
 
@@ -88,6 +125,11 @@ public:
         // vector<vector<int> > dp(4 , vector<int> (obstacles.size() , -1));
         // return solveMem(obstacles , 2 , 0 , dp);
 
-        return solveTab(obstacles);
+        //return solveTab(obstacles);
+
+        //Spaceoptimization
+
+        return solveSpace(obstacles);
+
     }
 };
